74eb24f337412215b489f33707b9bbdd232df893
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-02-17 15:56:57 +0200

    Rendering icons for game platforms

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,7 +7,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} gap="10px" padding="10px">
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing="10px" padding="10px">
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
