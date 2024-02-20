fc7b0a13233c4c6f8601d346c42ac2081d299602
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-02-20 20:23:42 +0200

    Filtering games by platform

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import { Genre } from "../hooks/useGenres";
 import CardSkeleton from "./CardSkeleton";
 import GameCard from "./GameCard";
@@ -7,10 +7,11 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 
 interface Props {
   selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
 }
 
-const GameGrid = ({ selectedGenre }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
--- src/components/PlatformIcons.tsx
+++ src/components/PlatformIcons.tsx
@@ -28,7 +28,7 @@ const PlatformIcons = ({ platforms }: Props) => {
     android: FaAndroid,
     web: BsGlobe,
   };
-  
+
   return (
     <HStack marginY="10px">
       {platforms.map((platform) => (
