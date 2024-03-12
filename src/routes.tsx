112e5248712bd1992cab33b04292d905d5b1f1d8
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-03-12 17:26:52 +0200

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import GameDetailsPage from "./pages/GameDetailsPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage />,
     element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },
